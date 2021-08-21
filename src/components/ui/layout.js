9bdd85765e8f3acf28c249189aca5433d691cbc4
Mahmudul Hasan <mahmudul.online@gmail.com>
2021-08-21 11:45:57 +0600

    added gatsby and graphql

---
+++ src/components/ui/layout.js
@@ -0,0 +1,38 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+import Footer from "./footer"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query GetCategories {
+      allStrapiCategory {
+        edges {
+          node {
+            name
+            strapiId
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Header categories={data.allStrapiCategory.edges} />
+      <div style={{ marginBottom: "10rem" }} />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/ui/layout"
+import HeroBlock from "../components/home/HeroBlock"
+
+const IndexPage = () => (
+  <Layout>
+    <HeroBlock />
+  </Layout>
+)
+
+export default IndexPage
